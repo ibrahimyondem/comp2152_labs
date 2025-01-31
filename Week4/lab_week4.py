70c7f72c600ef95d056f34e0bdb9d444f9ac59b6
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-01-31 09:26:18 -0500

    Update the Lab4

--- Week4/lab_week4.py
+++ Week4/lab_week4.py
@@ -175,6 +175,18 @@ print("You got the loots, so organize your belt alphabetically:")
 belt.sort()
 print("Your belt: ", belt)
 
+# Lab 04 - Q8 - Use the belt
+print("You see a Monster in the distance! So, quickly use your first item!")
+first_item = belt.pop(0)
+if first_item in good_loot_options:
+    health_points = min(6, (health_points + 2))
+    print(f"You used{first_item} , and your health changed to {health_points} health points.")
+elif first_item in bad_loot_options:
+    health_points = max(0, (health_points - 2))
+    print(f"You used{first_item} , and your health changed to {health_points} health points.")
+else:
+    print(f"You used the {first_item}, but it was not helpful!")
+
 input("Analyze the roll (Press enter)")
 # Compare Player vs Monster's strength
 print("--- You are matched in strength: " + str(combat_strength == m_combat_strength))
