195f8d7ba1e721246bdd41b64a6cdda5497756f6
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-03-28 08:24:24 -0400

    Update Heart Class

--- week12/heart.py
+++ week12/heart.py
@@ -1,3 +1,10 @@
 # Heart class for composition
 class Heart:
-    pass
+    def _init__(self):
+        print("Composition: Heart is created")
+        self.beats_per_minute = 72
+    def beat(self):
+        print("Composition: Heart is beating...")
+
+    def __del__(self):
+        print("Composition: Heart is destroyed")
