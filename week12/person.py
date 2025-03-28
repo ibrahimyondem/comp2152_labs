6dd8f88b6ac4db0220b58abc7e9e59945173d9ef
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2025-03-28 08:34:34 -0400

    Person class

--- week12/person.py
+++ week12/person.py
@@ -14,16 +14,22 @@ class Person(Mammal):
 
         # Set the Person-specific fields
         print("Constructor: Adding the Person parts of a person")
+        self.name = p_name
+        self.__height = p_height
 
 
     def __del__(self):
         print("Destructor: The garbage collector is now deleting the person object")
-        
+        super(Person,self).__del__()
 
         # Complex getter for height
-
+    @property
+    def height(self):
+        return self.__height
     # Complex setter for height
-
+    @height.setter
+    def height(self, p_height):
+        self.__height = p_height
 
     # Lab 12 - Private or protected METHODS are not possible in python.
     # Lab 12 - This means that all methods are public and can be overridden in a derived class.
